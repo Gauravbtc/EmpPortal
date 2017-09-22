af3c960b19a4537fcd9499aa41f9f1021c93deee
gaurav <gauravm@thinkbiz.co.in>
2017-09-22 15:30:24 +0530

    login functinlaity

---
+++ src/pages/user/UserLoginPage.js
@@ -0,0 +1,12 @@
+import React ,{ Component } from 'react'
+import LoginUserContainer from '../../containers/userContainer/LoginUserContainer';
+
+
+class LoginUserPage extends Component{
+    render(){
+      return(
+        <LoginUserContainer />
+      );
+    }
+  }
+export default LoginUserPage
