5410c37045de00811622c028d11056676437fa5d
gaurav <gauravm@thinkbiz.co.in>
2017-10-12 18:15:34 +0530

    redux persist fixes

--- src/index.js
+++ src/index.js
@@ -6,31 +6,40 @@ import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import '../node_modules/react-fontawesome';
 import {createStore,applyMiddleware} from 'redux';
-import {persistStore, autoRehydrate} from 'redux-persist'
+//import {persistStore, autoRehydrate} from 'redux-persist'
 
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers/index_reducers';
 import { composeWithDevTools} from 'redux-devtools-extension';
 import { authenticated } from './actions/auth_action';
-import { authUser,authUserSuccess } from './actions/user_action';
+import { authUser,authUserSuccess,authUserFailure } from './actions/user_action';
 
-//const user = localStorage.getItem('user');
+const user = localStorage.getItem('user');
 
 const store = createStore(
   rootReducer,
   composeWithDevTools(
     applyMiddleware(thunk),
-    autoRehydrate()
   )
 );
-persistStore(store)
+//persistStore(store)
 
+let next = store.dispatch
 
-//  if(user) {
-//    store.dispatch(authenticated(user));
-//   console.log("i m in index");
-// }
+if(user) {
+  next = store.dispatch(authUser());
+  next.payload.then((response) => {
+    if(!response.error && response.status === 200){
+      store.dispatch(authUserSuccess(response.data));
+      store.dispatch(authenticated(user));
+    }
+    else{
+      store.dispatch(authUserFailure(response.data));
+    } 
+  })
+  console.log(next.payload);
+ }
 
 console.log("i m in index");
 ReactDOM.render(
@@ -40,3 +49,10 @@ ReactDOM.render(
   </Router>
 </Provider>,document.getElementById('root'));
 registerServiceWorker();
+
+// function mapStateToProps(state, ownProps){
+//   return{
+//     loginUser: state.user.loginUser,
+//     params: ownProps
+//   }
+// }
